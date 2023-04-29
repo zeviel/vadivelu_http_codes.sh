d3c956b5f6d7799ce34aecad3509d83cb947693a
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-30 00:08:28 +0800

    Update vadivelu_http_codes.sh

--- src/vadivelu_http_codes.sh
+++ src/vadivelu_http_codes.sh
@@ -3,7 +3,7 @@
 api="https://vadivelu.anoram.com"
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
-function get_random_bear() {
+function get_http_code() {
   # 1 - format: (string): <format - default: jpg>
   # 2 - status_code: (integer): <status_code>
   # 3 - grayscale: (boolean): <true, false - default: false>
